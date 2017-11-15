1902d5c838db8715cf50447f6f99fbca5c1888f6
viniciusbsi <ifc.vinicius@gmail.com>
2017-11-15 21:53:50 -0200

    correção compo matrícula aluno

--- static/js/scp.js
+++ static/js/scp.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     $('.hora input').mask('00:00', {placeholder:'HH:MM'});
     $('.telefone input').mask('(00) 00000-0000', {placeholder:'(XX) XXXXX-XXXX'});
     $('.ano input').mask('00000', {placeholder:'Somente números'});
-    $('.matricula input').mask('00000000000000000000', {placeholder:'Somente números'});
+    $('.matricula input').mask('0000000000', {placeholder:'Somente números'});
 
     $('.message .close')
         .on('click', function () {
